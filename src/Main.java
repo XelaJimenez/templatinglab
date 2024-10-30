e5c84fc9a7280c5a41e5df06768722c69c6cd4a8
Alex <136139053+XelaJimenez@users.noreply.github.com>
2024-10-30 12:57:15 -0500

    good start

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
+import java.util.*;
+
 interface Ingredient {
     public String getName();
-    public int getQuantity();
+    public double getQuantity();
+}
+class SolidIngredient implements Ingredient {
+    private String _name;
+    private double _quantity;
+    public SolidIngredient(String name, double quantity) {
+        _name = name;
+        _quantity = quantity;
+    }
+    public String getName() {
+        return _name;
+    }
+    public double getQuantity() {
+        return _quantity;
+    }
+}
+class LiquidIngredient implements Ingredient {
+    private String _name;
+    private double _quantity;
+    public LiquidIngredient(String name, double quantity) {
+        _name = name;
+        _quantity = quantity;
+    }
+    public String getName() {
+        return _name;
+    }
+    public double getQuantity() {
+        return _quantity;
+    }
+}
+class Recipe<T extends Ingredient> {
+    private String _name;
+    private String _instructions;
+    private ArrayList<T> _ingredients;
+    public Recipe(String name, String instructions, ArrayList<T> ingredients) {
+        _name = name;
+        _instructions = instructions;
+        _ingredients = ingredients;
+    }
+    public void addIngredient(T item) {
+        _ingredients.add(item);
+    }
+    public void print() {
+        System.out.println(_ingredients);
+    }
 }
 public class Main {
     public static void main(String[] args) {
